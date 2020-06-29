eda5316252732e6e5921b001892c4760d2b7844a
lukeg <lukeg@DESKTOP-NORVVCH>
2020-06-30 03:03:37 +1200

    Add project files.

---
+++ HorseBetRace/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace HorseBetRace
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblMaxBet = new System.Windows.Forms.Label();
+            this.lblMinimumBet = new System.Windows.Forms.Label();
+            this.rbAl = new System.Windows.Forms.RadioButton();
+            this.rbBob = new System.Windows.Forms.RadioButton();
+            this.rbJoe = new System.Windows.Forms.RadioButton();
+            this.pbRaceTrack = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.lblBettor = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRaceTrack)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Controls.Add(this.numericUpDown2);
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.lblBettor);
+            this.panel1.Controls.Add(this.lblMaxBet);
+            this.panel1.Controls.Add(this.lblMinimumBet);
+            this.panel1.Controls.Add(this.rbAl);
+            this.panel1.Controls.Add(this.rbBob);
+            this.panel1.Controls.Add(this.rbJoe);
+            this.panel1.Location = new System.Drawing.Point(39, 296);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(678, 182);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblMaxBet
+            // 
+            this.lblMaxBet.AutoSize = true;
+            this.lblMaxBet.Location = new System.Drawing.Point(159, 55);
+            this.lblMaxBet.Name = "lblMaxBet";
+            this.lblMaxBet.Size = new System.Drawing.Size(35, 13);
+            this.lblMaxBet.TabIndex = 15;
+            this.lblMaxBet.Text = "label3";
+            this.lblMaxBet.Visible = false;
+            // 
+            // lblMinimumBet
+            // 
+            this.lblMinimumBet.AutoSize = true;
+            this.lblMinimumBet.Location = new System.Drawing.Point(24, 26);
+            this.lblMinimumBet.Name = "lblMinimumBet";
+            this.lblMinimumBet.Size = new System.Drawing.Size(35, 13);
+            this.lblMinimumBet.TabIndex = 9;
+            this.lblMinimumBet.Text = "Bettor";
+            // 
+            // rbAl
+            // 
+            this.rbAl.AutoSize = true;
+            this.rbAl.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.rbAl.Location = new System.Drawing.Point(25, 100);
+            this.rbAl.Margin = new System.Windows.Forms.Padding(2);
+            this.rbAl.Name = "rbAl";
+            this.rbAl.Size = new System.Drawing.Size(34, 17);
+            this.rbAl.TabIndex = 8;
+            this.rbAl.TabStop = true;
+            this.rbAl.Text = "Al";
+            this.rbAl.UseVisualStyleBackColor = false;
+            // 
+            // rbBob
+            // 
+            this.rbBob.AutoSize = true;
+            this.rbBob.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.rbBob.Location = new System.Drawing.Point(25, 77);
+            this.rbBob.Margin = new System.Windows.Forms.Padding(2);
+            this.rbBob.Name = "rbBob";
+            this.rbBob.Size = new System.Drawing.Size(44, 17);
+            this.rbBob.TabIndex = 7;
+            this.rbBob.TabStop = true;
+            this.rbBob.Text = "Bob";
+            this.rbBob.UseVisualStyleBackColor = false;
+            // 
+            // rbJoe
+            // 
+            this.rbJoe.AutoSize = true;
+            this.rbJoe.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.rbJoe.Location = new System.Drawing.Point(25, 55);
+            this.rbJoe.Margin = new System.Windows.Forms.Padding(2);
+            this.rbJoe.Name = "rbJoe";
+            this.rbJoe.Size = new System.Drawing.Size(42, 17);
+            this.rbJoe.TabIndex = 6;
+            this.rbJoe.TabStop = true;
+            this.rbJoe.Text = "Joe";
+            this.rbJoe.UseVisualStyleBackColor = false;
+            // 
+            // pbRaceTrack
+            // 
+            this.pbRaceTrack.BackgroundImage = global::HorseBetRace.MainResource.racetrack;
+            this.pbRaceTrack.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbRaceTrack.Location = new System.Drawing.Point(39, 12);
+            this.pbRaceTrack.Name = "pbRaceTrack";
+            this.pbRaceTrack.Size = new System.Drawing.Size(678, 278);
+            this.pbRaceTrack.TabIndex = 1;
+            this.pbRaceTrack.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::HorseBetRace.MainResource.Horse1;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(56, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(79, 41);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::HorseBetRace.MainResource.Horse2;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(56, 88);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(79, 41);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = global::HorseBetRace.MainResource.Horse3;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(56, 150);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(79, 41);
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.BackgroundImage = global::HorseBetRace.MainResource.Horse4;
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox4.Location = new System.Drawing.Point(56, 213);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(79, 38);
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            // 
+            // lblBettor
+            // 
+            this.lblBettor.AutoSize = true;
+            this.lblBettor.Location = new System.Drawing.Point(100, 122);
+            this.lblBettor.Name = "lblBettor";
+            this.lblBettor.Size = new System.Drawing.Size(23, 13);
+            this.lblBettor.TabIndex = 16;
+            this.lblBettor.Text = "Bet";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 148);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Cash on horse number";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(129, 120);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 18;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(129, 146);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 490);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pbRaceTrack);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRaceTrack)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton rbAl;
+        private System.Windows.Forms.RadioButton rbBob;
+        private System.Windows.Forms.RadioButton rbJoe;
+        private System.Windows.Forms.Label lblMaxBet;
+        private System.Windows.Forms.Label lblMinimumBet;
+        private System.Windows.Forms.PictureBox pbRaceTrack;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label lblBettor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
