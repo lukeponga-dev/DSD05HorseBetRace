01a37c79e6f5f8e1ebb51b06fa6c3422bf274b65
lukeg <lukeg@DESKTOP-NORVVCH>
2020-07-07 16:59:14 +1200

    Override

--- HorseBetRace/Data/Al.cs
+++ HorseBetRace/Data/Al.cs
@@ -5,42 +5,16 @@ namespace HorseBetRace.Data.AllPunters
 
     public class Al : Punter
     {
-        private Punter _punterImplementation;
-
         public Al() // Sets up the punter Al
         {
             PunterName = "Al";
             Cash = 50;
         }
 
-        public override string PunterName
-        {
-            get => _punterImplementation.PunterName;
-            set => _punterImplementation.PunterName = value;
-        }
-
-        public override Bet MyBet
-        {
-            get => _punterImplementation.MyBet;
-            set => _punterImplementation.MyBet = value;
-        }
-
-        public override int Cash
-        {
-            get => _punterImplementation.Cash;
-            set => _punterImplementation.Cash = value;
-        }
-
-        public override RadioButton MyRadioButton
-        {
-            get => _punterImplementation.MyRadioButton;
-            set => _punterImplementation.MyRadioButton = value;
-        }
-
-        public override Label MyLabel
-        {
-            get => _punterImplementation.MyLabel;
-            set => _punterImplementation.MyLabel = value;
-        }
+        public override string PunterName { get; set; }
+        public override Bet MyBet { get; set; }
+        public override int Cash { get; set; }
+        public override RadioButton MyRadioButton { get; set; }
+        public override Label MyLabel { get; set; }
     }
 }
--- HorseBetRace/Data/Joe.cs
+++ HorseBetRace/Data/Joe.cs
@@ -4,42 +4,17 @@ namespace HorseBetRace.Data.AllPunters
 {
     public class Joe : Punter
     {
-        private Punter _punterImplementation;
-
         public Joe() // Sets up the punter Joe
         {
             PunterName = "Joe";
             Cash = 50;
         }
 
-        public override string PunterName
-        {
-            get => _punterImplementation.PunterName;
-            set => _punterImplementation.PunterName = value;
-        }
+        public override string PunterName { get; set; }
+        public override Bet MyBet { get; set; }
+        public override int Cash { get; set; }
+        public override RadioButton MyRadioButton { get; set; }
+        public override Label MyLabel { get; set; }
 
-        public override Bet MyBet
-        {
-            get => _punterImplementation.MyBet;
-            set => _punterImplementation.MyBet = value;
-        }
-
-        public override int Cash
-        {
-            get => _punterImplementation.Cash;
-            set => _punterImplementation.Cash = value;
-        }
-
-        public override RadioButton MyRadioButton
-        {
-            get => _punterImplementation.MyRadioButton;
-            set => _punterImplementation.MyRadioButton = value;
-        }
-
-        public override Label MyLabel
-        {
-            get => _punterImplementation.MyLabel;
-            set => _punterImplementation.MyLabel = value;
-        }
     }
 }
